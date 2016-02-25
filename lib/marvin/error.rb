b6fb00e3302a117b23b3eb8293afc558d07a93e6
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-02-24 19:39:02 -0500

    Let ParserError take in either a single kind or multiple kinds (resolves #6).

--- lib/marvin/error.rb
+++ lib/marvin/error.rb
@@ -31,7 +31,7 @@ module Marvin
     # Creates a new Lexer error.
     #
     # @param [Marvin::Token] token The token we're having issues with.
-    # @param [Symbol] kind The kind expected.
+    # @param [Symbol, Array<Symbol>] kind The kind (or kinds) expected.
     # @return [Marvin::Error] The error.
     def initialize(token, kind)
       @token = token
@@ -40,6 +40,12 @@ module Marvin
 
     # The message for the error.
     def message
+      if @kind.is_a?(Array)
+        @kind_str = @kind.map(&:to_s).map(&:upcase).join(' or ')
+      else
+        kind_str = @kind.to_s.upcase
+      end
+
       "Expected #{@kind.to_s.upcase}, found #{@token.kind.to_s.upcase} \"#{@token.lexeme}\" on line #{@token.attributes[:line]} at character #{@token.attributes[:char]}"
     end
   end
