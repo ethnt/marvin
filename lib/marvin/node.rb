1018521de25853f3fc4bc2410fd2e59f94036db3
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2016-04-25 10:42:31 -0400

    Cleaned up the symbol table generation process.

--- lib/marvin/node.rb
+++ lib/marvin/node.rb
@@ -7,58 +7,54 @@ module Marvin
   class Node < ::Tree::TreeNode
     attr_reader :children_hash
 
+    # Creates a new Node. Automatically sets the name to a random string.
+    #
+    # @param [Object] content Whatever content that should be in the node.
+    # @return [Marvin::Node] The new node.
     def initialize(content)
       super(SecureRandom.hex, content)
     end
 
+    # Checks to see if the content is a Production.
+    #
+    # @return [Boolean] Whether or not this contains a Production.
     def production?
       @content.is_a?(Production)
     end
 
+    # Checks to see if the content is a Token.
+    #
+    # @return [Boolean] Whether or not this contains a Token.
     def token?
       @content.is_a?(Token)
     end
 
-    def find_child(_name)
-      @children_hash[name]
-    end
-
+    # Resolves the type of the content of this node. If it contains a
+    # Production, it will continue to search the right-most child until it
+    # reaches an actual value.
+    #
+    # @param [Marvin::Scope] scope The scope to search through.
+    # @return [String] The type (+int+, +boolean+, or +string+).
     def resolve_type(scope)
+
       # Resolve the type of any children if it's a production
       return @children.last.resolve_type(scope) if production?
 
-      _kind = @content.kind
+      # Convert the kind to a token.
+      type = case @content.kind
+              when :char
+                scope.find_identifier(content.lexeme).type
+              when :digit
+                'int'
+              when :boolval
+                'boolean'
+              when :string
+                'string'
+              else
+                _kind
+              end
 
-      # look up variable references
-      if _kind == :char
-        _kind = scope.find_identifier(content.lexeme).type
-      end
-
-      if %i(digit boolval string).include?(_kind)
-        types = {
-          digit: 'int',
-          boolval: 'boolean',
-          string: 'string'
-        }
-
-        return types[_kind]
-      else
-        return _kind
-      end
-    end
-
-    # Finds in the current nodes children and any of the ancestors.
-    def find_in_parentage(_name)
-      result = nil
-
-      parentage.each do |parent|
-        result = parent.children_hash[_name]
-
-        break if result
-      end
-
-      # search.empty? ? nil : search.first
-      result
+      type
     end
   end
 end
--- lib/marvin/symbol_table.rb
+++ lib/marvin/symbol_table.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Marvin
 
   # Contains scopes and their child variable types, keys, and values.
@@ -21,58 +23,84 @@ module Marvin
       node.children.each do |child|
         next unless child.production?
 
-        production = child.content
+        # Handle the type of production to get the next scope.
+        next_scope =  case child.content.name
+                      when 'Block'
+                        handle_block!(child, scope)
+                      when 'VariableDeclaration'
+                        handle_variable_declaration!(child, scope)
+                      when 'Assignment'
+                        handle_assignment!(child, scope)
+                      else
+                        scope
+                      end
 
-        case production.name
+        binding.pry if scope.nil?
 
-        # If we reach a new block, let's create a new scope.
-        # if production.name == 'Block'
-        when 'Block'
-          nested_scope = Marvin::Scope.new('<Scope>')
-
-          # Add the nested scope to the current scope and set the current scope
-          # to the nested scope.
-          scope << nested_scope
-          scope = nested_scope
-
-        # If we reach a variable declaration, we're adding a new identifier to
-        # the current scope.
-        when 'VariableDeclaration'
-          type = child.children.first.content.lexeme
-          name = child.children.last.content.lexeme
-
-          if scope.find_identifier(name, current_only: true)
-            token = child.children.last.content
-
-            @config.logger.warning("Redeclared identifier at #{token.lexeme} on line #{token.attributes[:line]} at character #{token.attributes[:char]}")
-          end
-
-          identifier = Marvin::Node.new(Marvin::Identifier.new(name: name, type: type))
-
-          scope.add(identifier)
-
-        # If we reach an assignment statement, we're going to check the type
-        when 'Assignment'
-          name = child.children.first.content.lexeme
-
-          # Find the previous variable declaration.
-          identifier = scope.find_identifier(name)
-
-          # If we can't find an identifier, throw an error!
-          return Marvin::Error::ScopeError.new(child.children.first.content) unless identifier
-
-          declared_type = identifier.type
-          given_type = child.children.last.resolve_type(scope)
-
-          return Marvin::Error::TypeError.new(child.children.last, declared_type, given_type) if declared_type != given_type
-
-          identifier = Marvin::Node.new(Marvin::Identifier.new(name: name, type: type))
-
-          scope.add(identifier)
-        end
-
-        traverse_ast(child, scope)
+        traverse_ast(child, next_scope)
       end
+    end
+
+    # If we reach a new block, let's create a new scope.
+    # if production.name == 'Block'
+    #
+    # @param [Marvin::Node] node The node to search through.
+    # @param [Marvin::Scope] scope The current scope.
+    def handle_block!(node, scope)
+      nested_scope = Marvin::Scope.new('<Scope>')
+
+      # Add the nested scope to the current scope and set the current scope
+      # to the nested scope.
+      scope.add(nested_scope)
+
+      nested_scope
+    end
+
+    # If we reach a variable declaration, we're adding a new identifier to
+    # the current scope.
+    #
+    # @param [Marvin::Node] node The node to search through.
+    # @param [Marvin::Scope] scope The current scope.
+    def handle_variable_declaration!(node, scope)
+      type = node.children.first.content.lexeme
+      name = node.children.last.content.lexeme
+
+      if scope.find_identifier(name, current_only: true)
+        token = node.children.last.content
+
+        @config.logger.warning("Redeclared identifier at #{token.lexeme} on line #{token.attributes[:line]} at character #{token.attributes[:char]}")
+      end
+
+      identifier = Marvin::Node.new(Marvin::Identifier.new(name: name, type: type))
+
+      scope.add(identifier)
+
+      scope
+    end
+
+    # If we reach an assignment statement, we're going to check the type.
+    #
+    # @param [Marvin::Node] node The node to search through.
+    # @param [Marvin::Scope] scope The current scope.
+    def handle_assignment!(node, scope)
+      name = node.children.first.content.lexeme
+
+      # Find the previous variable declaration.
+      identifier = scope.find_identifier(name)
+
+      # If we can't find an identifier, throw an error!
+      return Marvin::Error::ScopeError.new(node.children.first.content) unless identifier
+
+      declared_type = identifier.type
+      given_type = node.children.last.resolve_type(scope)
+
+      return Marvin::Error::TypeError.new(node.children.last, declared_type, given_type) if declared_type != given_type
+
+      identifier = Marvin::Node.new(Marvin::Identifier.new(name: name, type: given_type))
+
+      scope.add(identifier)
+
+      scope
     end
 
     # Print out the tree.
